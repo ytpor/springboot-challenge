0ec060b534a136295ce618ff59b34cc586141926
YT Por <agtong+ytpor@gmail.com>
2025-04-28 16:58:49 +0800

    Add serialVersionUID to class.

--- src/main/java/com/ytpor/api/exception/DuplicateRecordException.java
+++ src/main/java/com/ytpor/api/exception/DuplicateRecordException.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateRecordException extends RuntimeException {
+
+    private static final long serialVersionUID = -4279217446248389504L;
+
     public DuplicateRecordException(String exception) {
         super(exception);
     }
